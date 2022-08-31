f601dd38ef9a8ccd8f4b5a80e983c37fc46c33a4
ihababbas <ihababbas26@gmail.com>
2022-08-31 11:34:15 +0300

    done

--- src/movie.js
+++ src/movie.js
@@ -1,33 +1,29 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from'react';
+import Card from 'react-bootstrap/Card'
 
 
 class Movie extends React.Component {
   render() {
-    return (
+    
+    let display = this.props.movieData.map(x => {
+        return (<Card  className="card" style={{ width: "18rem"  }}>
+          <Card.Title>{x.title}</Card.Title>
+          <Card.Img src={`${x.imgPath}`} />
+          <Card.Text>{x.overview}</Card.Text>
+          <Card.Text>{x.avgVote}</Card.Text>
+          <Card.Text>{x.sumVote}</Card.Text>
+          <Card.Text>{x.popularity}</Card.Text>
+          <Card.Text>{x.release}</Card.Text>
+        </Card>)
+    })
+console.log(display)
+    return(
       <>
-        {this.props.movieData.map((item) => {
-          return (
-            
-              <Card className="card" style={{ width: "18rem"  }}>
-                <Card.Img className="img" variant="top" src={item.poster_path} />
-                <Card.Body>
-                  <Card.Title>{item.title}</Card.Title>
-                  <Card.Text>
-                    <p><span>Overview:</span> {item.overview}</p>
-                    <p><span>Release Date:</span>  {item.release_date}</p>
-                    <p><span>Average Vote:</span>  {item.vote_average}</p>
-                    <p><span>Total Votes:</span>  {item.vote_count}</p>
-                    <p><span>Popularity:</span>  {item.popularity}</p>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              
-          );
-        })}
+      
+        {display}
+      
       </>
-    );
+    )
   }
 }
 
